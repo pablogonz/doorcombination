55ef0f1c0075f75a793cc26a429cb0bd18c8f96f
pablogonz <65257437+pablogonz@users.noreply.github.com>
2021-11-29 21:40:20 -0400

    push

---
+++ doorcombination/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doorcombination
+{
+   public class doorcomb
+    {
+        public static bool doorstatus(string key)
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ doorcombinationtest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace doorcombinationtest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void validate_door_status_open()
+        {
+
+        }
+        [TestMethod]
+        public void validate_door_status_closed()
+        {
+
+        }
+        [TestMethod]
+        public void validate_door_status_locked()
+        {
+
+        }
+        [TestMethod]
+        public void validate_door_status_unlocked()
+        {
+
+        }
+    }
+}
